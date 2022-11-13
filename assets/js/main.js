5c07b4e3906dc04545d98b1d41ce1b73998f0378
Ömer Düzgün <Cecemaru@hotmail.com>
2022-11-13 21:19:13 +0300

    added a scroll button up js and css properties were encoded

--- assets/js/main.js
+++ assets/js/main.js
@@ -128,3 +128,13 @@ const sendEmail = (e) => {
   }
 };
 contactForm.addEventListener("submit", sendEmail);
+
+/* =============== SHOW SCROLL UP ============== */
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is hiher than 350 viewport
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
