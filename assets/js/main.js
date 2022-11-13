d906cdafd8d7fc0df3d5b7c0e1a5a6c1175221cc
Ömer Düzgün <Cecemaru@hotmail.com>
2022-11-13 16:24:56 +0300

    the main.js page is calculate js encoded for the calculate submit button

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,57 @@ const scrollHeader = () => {
     : header.classList.remove("bg-header");
 };
 window.addEventListener("scroll", scrollHeader);
+
+/* ============== CALCULATE BMI JS ================== */
+const calculateForm = document.getElementById("calculate-form"),
+  calculateCm = document.getElementById("calculate-cm"),
+  calculateKg = document.getElementById("calculate-kg"),
+  calculateMessage = document.getElementById("calculate-message");
+
+const calculateBmi = (e) => {
+  e.preventDefault();
+
+  // Check if the fields have a value
+  if (calculateCm.value == "" || calculateKg.value == "") {
+    // Add and remove color
+    calculateMessage.classList.remove("color-green");
+    calculateMessage.classList.add("color-red");
+
+    // Show message
+    calculateMessage.textContent = "Fill in the Height and Weight";
+
+    // Remove message three seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 3000);
+  } else {
+    // BMI Formul
+    const cm = calculateCm.value / 100,
+      kg = calculateKg.value,
+      bmi = Math.round(kg / (cm * cm));
+
+    // Show your health status
+    if (bmi < 18.5) {
+      // Add color and display message
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are Skinny`;
+    } else if (bmi < 25) {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are Healthy`;
+    } else {
+      calculateMessage.classList.add("color-green");
+      calculateMessage.textContent = `Your BMI is ${bmi} and you are Overweight`;
+    }
+
+    // To clear the input field
+    calculateKg.value = "";
+    calculateCm.value = "";
+
+    // Remove message four seconds
+    setTimeout(() => {
+      calculateMessage.textContent = "";
+    }, 4000);
+  }
+};
+
+calculateForm.addEventListener("submit", calculateBmi);
